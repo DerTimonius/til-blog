b9d0eb3f543b20b417f911af1d3412001173e350
Timon Jurschitsch <103483059+DerTimonius@users.noreply.github.com>
2023-10-26 18:25:25 +0200

    feat: add reading time (#3)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-
 import mdx from '@astrojs/mdx';
+import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config';
+import { remarkReadingTime } from './remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx({ remarkPlugins: [remarkReadingTime] })],
 });
