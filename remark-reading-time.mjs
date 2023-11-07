d2583143571e55f2ee6612ec2c1470b40c90f6ff
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-11-07 17:33:44 +0100

    add autofocus post

--- remark-reading-time.mjs
+++ remark-reading-time.mjs
@@ -4,7 +4,7 @@ import getReadingTime from 'reading-time';
 export function remarkReadingTime() {
   return function (tree, { data }) {
     const textOnPage = toString(tree);
-    const readingTime = getReadingTime(textOnPage, { wordsPerMinute: 120 });
+    const readingTime = getReadingTime(textOnPage, { wordsPerMinute: 150 });
     // readingTime.text will give us minutes read as a friendly string,
     // i.e. "3 min read"
     data.astro.frontmatter.minutesRead = readingTime.text;
