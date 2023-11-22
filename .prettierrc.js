76bbb084ac89fec7d5178880550d3c6c4db8ab1f
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-11-22 09:48:46 +0100

    add svelte packages

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,16 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ['prettier-plugin-astro'],
+  plugins: ['prettier-plugin-astro', 'prettier-plugin-svelte'],
   overrides: [
     {
       files: '*.astro',
       options: {
         parser: 'astro',
       },
+    },
+    {
+      files: '*.svelte',
+      options: { parser: 'svelte' },
     },
   ],
   singleQuote: true,
