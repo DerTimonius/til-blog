827a13b88b72a646618ee4e4369ada97ef27bfea
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-11-22 09:49:00 +0100

    add search functionality

---
+++ src/components/SearchBar.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  // @ts-ignore
+  import type { CollectionEntry } from 'astro:content';
+  import Fuse from 'fuse.js';
+  import SearchResults from './SearchResults.svelte';
+
+  const { posts } = $props<{ posts: CollectionEntry<'blogs'>[] }>();
+  const searchList = posts.map((p) => {
+    return {
+      title: p.data.title,
+      description: p.data.description,
+      data: p.data,
+      slug: p.slug,
+      tags: p.data.tags,
+    };
+  });
+  const fuse = new Fuse(searchList, {
+    keys: ['title', 'description', 'tags'],
+    includeMatches: true,
+    minMatchCharLength: 2,
+    threshold: 0.3,
+  });
+  let searchTerm = $state('');
+  let foundPosts: CollectionEntry<'blogs'>[] = $state([]);
+
+  const searchPosts = () => {
+    if (searchTerm.length > 2) {
+      foundPosts = fuse.search(
+        searchTerm,
+      ) as unknown as CollectionEntry<'blogs'>[];
+    }
+    console.log('foundPosts', foundPosts);
+  };
+</script>
+
+<label class="relative block">
+  <span class="absolute inset-y-0 left-0 flex items-center pl-2 opacity-75">
+    <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+      <path
+        d="M19.023 16.977a35.13 35.13 0 0 1-1.367-1.384c-.372-.378-.596-.653-.596-.653l-2.8-1.337A6.962 6.962 0 0 0 16 9c0-3.859-3.14-7-7-7S2 5.141 2 9s3.14 7 7 7c1.763 0 3.37-.66 4.603-1.739l1.337 2.8s.275.224.653.596c.387.363.896.854 1.384 1.367l1.358 1.392.604.646 2.121-2.121-.646-.604c-.379-.372-.885-.866-1.391-1.36zM9 14c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z"
+      ></path>
+    </svg>
+  </span>
+  <input
+    class="block w-full rounded border border-skin-fill
+  border-opacity-40 bg-skin-fill py-3 pl-10
+  pr-3 placeholder:italic placeholder:text-opacity-75
+  focus:border-skin-accent focus:outline-none"
+    placeholder="Search for anything..."
+    name="search"
+    bind:value={searchTerm}
+    on:input={searchPosts}
+  />
+</label>
+
+{#if searchTerm.length > 2 && foundPosts.length === 0}
+  <p>Sorry, I wasn't able to find anything!</p>
+{:else if foundPosts.length}
+  <SearchResults {foundPosts} />
+{/if}
---
+++ src/components/SearchResults.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  const { foundPosts } = $props<{ foundPosts: any }>();
+
+  console.log('foundPosts in other component', foundPosts);
+</script>
+
+<ol class="list-none gap-6">
+  {#each foundPosts as foundPost}
+    <li class="my-6">
+      <a
+        href={`/blog/${foundPost.item.slug}`}
+        class="inline-block text-lg font-medium text-skin-accent decoration-dashed underline-offset-4 focus-visible:no-underline focus-visible:underline-offset-0"
+        data-test-id={`search-result-${foundPost.item.slug}`}
+      >
+        <h2>{foundPost.item.title}</h2>
+      </a>
+      <p>{foundPost.item.description}</p>
+    </li>
+  {/each}
+</ol>
