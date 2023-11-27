78896c146501ba764ae5f42fae67609406354ef4
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2023-11-27 10:30:06 +0100

    fix flaky test

--- playwright/search.spec.ts
+++ playwright/search.spec.ts
@@ -6,7 +6,10 @@ test('search functionality', async ({ page }) => {
   await page.locator(`[data-test-id="nav-link-search"]`).click();
   await expect(page).toHaveURL('/search');
 
+  await page.waitForTimeout(300);
   await page.locator('input').fill('javascript');
+  await page.keyboard.press("Enter");
+  await page.waitForTimeout(300);
   await expect(page.locator(`[data-test-id^="search-result"]`)).not.toHaveCount(
     0,
   );
