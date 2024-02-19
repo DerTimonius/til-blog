18dd6d7f4e0aaad80f9595d9d499c7bd8c49111f
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2024-02-19 13:29:39 +0100

    style: use custom themes for expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,24 @@
+import fs from 'node:fs';
+
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
-import expressiveCode from 'astro-expressive-code';
+import expressiveCode, { ExpressiveCodeTheme } from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 
 import { pluginErrorPreview } from './src/plugins/error-preview-plugin';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
+
+const catppuccinJsoncString = fs.readFileSync(
+  new URL('./theme.jsonc', import.meta.url),
+  'utf-8',
+);
+const catppuccin = ExpressiveCodeTheme.fromJSONString(catppuccinJsoncString);
+
+const synthwaveJsoncString = fs.readFileSync(
+  new URL('./theme2.jsonc', import.meta.url),
+  'utf-8',
+);
+const synthwave = ExpressiveCodeTheme.fromJSONString(synthwaveJsoncString);
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,13 +26,8 @@ export default defineConfig({
     tailwind(),
     expressiveCode({
       plugins: [pluginErrorPreview()],
-      styleOverrides: {
-        frames: {
-          editorTabBarBackground: ({ theme }) =>
-            theme.colors['tab.activeBackground'],
-        },
-      },
-      themes: ['poimandres'],
+
+      themes: [catppuccin, synthwave],
     }),
     svelte(),
   ],
