89fcaf4e23d88eff266ccf03077ebd2a06af8c30
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2024-04-01 10:53:04 +0200

    change back to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,16 @@
 import fs from 'node:fs';
-
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
 import expressiveCode, { ExpressiveCodeTheme } from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
-
 import { pluginErrorPreview } from './src/plugins/error-preview-plugin';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
-
+import mdx from '@astrojs/mdx';
 const catppuccinJsoncString = fs.readFileSync(
   new URL('./theme.jsonc', import.meta.url),
   'utf-8',
 );
 const catppuccin = ExpressiveCodeTheme.fromJSONString(catppuccinJsoncString);
-
 const synthwaveJsoncString = fs.readFileSync(
   new URL('./theme2.jsonc', import.meta.url),
   'utf-8',
@@ -32,6 +29,9 @@ export default defineConfig({
       useDarkModeMediaQuery: true,
     }),
     svelte(),
+    mdx({ remarkPlugins: [remarkReadingTime] }),
   ],
-  markdown: { remarkPlugins: [remarkReadingTime] },
+  // markdown: {
+  //   remarkPlugins: [remarkReadingTime]
+  // }
 });
