07cc885cd24d5b70237b64310b49bf6c688af94d
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2024-04-10 17:35:31 +0200

    feat: specify future release date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -3,3 +3,7 @@ import dayjs from 'dayjs';
 export function formatDate(date: Date | string): string {
   return dayjs(date).format('MMMM DD, YYYY');
 }
+
+export function checkReleaseDate(date: Date | string): boolean {
+  return dayjs(date).isBefore(dayjs());
+}
