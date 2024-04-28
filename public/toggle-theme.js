72b530c37bc083db1572e31ae7690412784d7f87
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2024-04-28 10:01:59 +0200

    fix(a11y): dont autoanimate when reduced motion is set

--- public/toggle-theme.js
+++ public/toggle-theme.js
@@ -1,14 +1,9 @@
-const primaryColorScheme = ''; // "light" | "dark"
-
 // Get theme data from local storage
 const currentTheme = localStorage.getItem('theme');
 
 function getPreferTheme() {
   // return theme value in local storage if it is set
   if (currentTheme) return currentTheme;
-
-  // return primary color scheme if it is set
-  if (primaryColorScheme) return primaryColorScheme;
 
   // return user device's prefer color scheme
   return window.matchMedia('(prefers-color-scheme: dark)').matches
@@ -34,23 +29,6 @@ function reflectPreference() {
 
   document.querySelector('#theme-btn')?.setAttribute('aria-label', themeValue);
   document.querySelector('#theme-btn2')?.setAttribute('aria-label', themeValue);
-
-  // Get a reference to the body element
-  const body = document.body;
-
-  // Check if the body element exists before using getComputedStyle
-  if (body) {
-    // Get the computed styles for the body element
-    const computedStyles = window.getComputedStyle(body);
-
-    // Get the background color property
-    const bgColor = computedStyles.backgroundColor;
-
-    // Set the background color in <meta theme-color ... />
-    document
-      .querySelector("meta[name='theme-color']")
-      ?.setAttribute('content', bgColor);
-  }
 }
 
 // set early so no page flashes / CSS is made aware
