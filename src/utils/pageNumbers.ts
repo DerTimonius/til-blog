2c57dd4eb90d8f816231da5f08224cde52f3f5d4
Timon Jurschitsch <103483059+DerTimonius@users.noreply.github.com>
2024-09-07 15:38:28 +0200

    chore: use new pagination options (#206)
    * chore: use new pagination options
    
    * fix tests
    
    * use global redirect config

--- src/utils/pageNumbers.ts
+++
@@ -1,7 +0,0 @@
-import { site } from './config';
-
-export default function getPageNumbers(numberOfPosts: number) {
-  const numberOfPages = Math.ceil(numberOfPosts / Number(site.postPerPage));
-
-  return Array.from({ length: numberOfPages }, (_, i) => i + 1);
-}
