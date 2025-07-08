953ff83d5cac8cf1edb0a1774908d150ad0ecac3
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2025-07-08 16:34:11 +0200

    chore: use built-in expressive code themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-import fs from 'node:fs';
 import svelte from '@astrojs/svelte';
 import tailwind from '@astrojs/tailwind';
-import expressiveCode, { ExpressiveCodeTheme } from 'astro-expressive-code';
+import expressiveCode from 'astro-expressive-code';
 import { defineConfig } from 'astro/config';
 import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
@@ -10,16 +9,6 @@ import mdx from '@astrojs/mdx';
 import { pluginErrorPreview } from './src/plugins/error-preview-plugin';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 import vercel from '@astrojs/vercel';
-const catppuccinJsoncString = fs.readFileSync(
-  new URL('./theme.jsonc', import.meta.url),
-  'utf-8',
-);
-const catppuccin = ExpressiveCodeTheme.fromJSONString(catppuccinJsoncString);
-const synthwaveJsoncString = fs.readFileSync(
-  new URL('./theme2.jsonc', import.meta.url),
-  'utf-8',
-);
-const synthwave = ExpressiveCodeTheme.fromJSONString(synthwaveJsoncString);
 
 // https://astro.build/config
 export default defineConfig({
@@ -35,10 +24,7 @@ export default defineConfig({
     tailwind(),
     expressiveCode({
       plugins: [pluginErrorPreview()],
-      themes: [catppuccin, synthwave],
-      themeCssRoot: 'html',
-      themeCssSelector: (theme) => `[data-theme=${theme.name}]`,
-      useDarkModeMediaQuery: true,
+      themes: ['catppuccin-macchiato', 'catppuccin-latte'],
     }),
     svelte(),
     mdx(),
