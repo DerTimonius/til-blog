9ce44c38c437eda9a91f53b6c2c8ef8d3e7fde48
Timon Jurschitsch <timon.jurschitsch@gmail.com>
2026-03-27 07:14:49 +0100

    feat: add magic move

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,47 +5,52 @@ import { rehypeHeadingIds } from '@astrojs/markdown-remark';
 import rehypeAutolinkHeadings from 'rehype-autolink-headings';
 import mdx from '@astrojs/mdx';
 
-import tailwindcss from "@tailwindcss/vite"
+import tailwindcss from '@tailwindcss/vite';
 import { pluginErrorPreview } from './src/plugins/error-preview-plugin';
 import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs';
 import vercel from '@astrojs/vercel';
-import { pluginLanguageLogo } from "ec-lang-logo"
+import { pluginLanguageLogo } from 'ec-lang-logo';
+import { pluginMagicMove } from 'ec-magic-move';
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: {
-    prefetchAll: true
-  },
+	prefetch: {
+		prefetchAll: true,
+	},
 
-  redirects: {
-    '/blog/': '/posts/1',
-  },
+	redirects: {
+		'/blog/': '/posts/1',
+	},
 
-  integrations: [
-    expressiveCode({
-      plugins: [pluginErrorPreview(), pluginLanguageLogo({
-        color: "mono",
-        excludedLangs: ["json"]
-      })],
-      themes: ['catppuccin-mocha', 'catppuccin-latte'],
-    }),
-    svelte(),
-    mdx(),
-  ],
+	integrations: [
+		expressiveCode({
+			plugins: [
+				pluginErrorPreview(),
+				pluginLanguageLogo({
+					color: 'mono',
+					excludedLangs: ['json'],
+				}),
+				pluginMagicMove({ theme: 'tokyo-night', duration: 400, stagger: 5 }),
+			],
+			themes: ['catppuccin-mocha', 'catppuccin-latte'],
+			themeCssSelector: (theme) => `[data-theme='${theme.type}']`,
+		}),
+		svelte(),
+		mdx(),
+	],
 
-  image: {
-    remotePatterns: [{protocol: "https", hostname: "**.bsky.app"}]
-  },
+	image: {
+		remotePatterns: [{ protocol: 'https', hostname: '**.bsky.app' }],
+	},
 
-  markdown: {
-    remarkPlugins: [remarkReadingTime],
-    rehypePlugins: [
-      rehypeHeadingIds,
-      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
-    ],
-  },
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		rehypePlugins: [
+			rehypeHeadingIds,
+			[rehypeAutolinkHeadings, { behavior: 'wrap' }],
+		],
+	},
 
-  adapter: vercel(),
-  vite: {plugins: [tailwindcss()]}
+	adapter: vercel(),
+	vite: { plugins: [tailwindcss()] },
 });
-
